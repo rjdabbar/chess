57cd48719b7186cb406b61a6778b3155b8c32d57
RJ and James <rj&james@gmail.com>
2015-08-04 12:22:47 -0400

    Add Board#[] and Board#[]= methods to Board.

--- board.rb
+++ board.rb
@@ -1,6 +1,19 @@
 class Board
   BOARD_SIZE = 8
+  attr_accessor :board
+  
   def initialize
     @board = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) }
   end
+
+  def [](pos)
+    x, y = pos
+    board[x][y]
+  end
+
+  def []=(pos, piece)
+    x, y = pos
+    self.board[x][y] = piece
+  end
+
 end

4072826b00565f69e360c02f2fbb9796723cf87c
RJ and James <rj&james@gmail.com>
2015-08-04 18:02:16 -0400

    Create sliding_piece.rb

---
+++ sliding_piece.rb
@@ -0,0 +1,52 @@
+
+class SlidingPiece < Piece
+  DIAGONAL_DELTAS = [
+    [-1, -1], [-1, 1], [1, 1], [1, -1]
+  ]
+  ORTHOGANAL_DELTAS = [
+    [0, -1], [0, 1], [1, 0], [-1, 0]
+  ]
+  def slide(pos, direction)
+    moves = []
+    next_pos = new_pos(pos, direction)
+    while (next_pos.all? &BOUND_PROC) && empty_or_enemy?(next_pos)
+      moves << next_pos
+      board[next_pos].nil? ? next_pos = new_pos(next_pos, direction) : break
+    end
+
+    moves
+  end
+
+  def moves
+    valid_moves = []
+    self.move_dirs.each do |delta|
+      valid_moves += slide(pos, delta)
+    end
+    valid_moves
+  end
+end
+
+
+class Queen < SlidingPiece
+  attr_reader :move_dirs
+  def initialize(pos, board, color)
+    super(pos, board, color)
+    @move_dirs = DIAGONAL_DELTAS + ORTHOGANAL_DELTAS
+  end
+end
+
+class Rook < SlidingPiece
+  attr_reader :move_dirs
+  def initialize(pos, board, color)
+    super(pos, board, color)
+    @move_dirs = ORTHOGANAL_DELTAS
+  end
+end
+
+class Bishop < SlidingPiece
+  attr_reader :move_dirs
+  def initialize(pos, board, color)
+    super(pos, board, color)
+    @move_dirs = DIAGONAL_DELTAS
+  end
+end

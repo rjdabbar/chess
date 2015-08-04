85dfb7eb0aa2c9d4251143714219ecd0598172ef
RJ and James <rj&james@gmail.com>
2015-08-04 18:11:36 -0400

    Add comments for record.

--- piece.rb
+++ piece.rb
@@ -1,10 +1,6 @@
 require_relative 'board.rb'
-# require_relative 'stepping_piece.rb'
-# require_relative 'sliding_piece.rb'
-# require_relative 'pawn.rb'
 
 class Piece
-
 
   BOUND_PROC = Proc.new { |coordinate| coordinate.between?(0,7) }
   PAWN_START_PROC = Proc.new { |coord| coord * 2 }
@@ -16,6 +12,7 @@ class Piece
     @pos, @board, @color = pos, board, color
   end
 
+######### MOVE THIS LOGIC ALL TO THE BOARD
   def empty_or_enemy?(next_pos)
     empty?(next_pos) || enemy?(next_pos)
   end
@@ -27,7 +24,7 @@ class Piece
   def enemy?(next_pos)
     !board[next_pos].nil? && (board[next_pos].color != self.color)
   end
-
+################ 
   def new_pos(start_pos, dir)
     [(start_pos[0] + dir[0]), (start_pos[1] + dir[1])]
   end

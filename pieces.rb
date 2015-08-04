72330ff999e6932e1e32d450d9b11a22a67e493b
RJ and James <rj&james@gmail.com>
2015-08-04 11:33:09 -0400

    Create additional piece subclasses.

--- pieces.rb
+++ pieces.rb
@@ -12,3 +12,18 @@ end
 
 class Pawn < Piece
 end
+
+class King < SteppingPiece
+end
+
+class Queen < SlidingPiece
+end
+
+class Rook < SlidingPiece
+end
+
+class Bishop < SlidingPiece
+end
+
+class Knight < SteppingPiece
+end
